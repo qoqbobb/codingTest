4080bd588627b7b797635ab57cb545fd0726a4eb
qoqbobb <kail853@naver.com>
2025-06-23 11:29:24 +0900

    06-23

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Level1_cutBill level1_cutBill = new Level1_cutBill();
-        int result = level1_cutBill.solution();
-        System.out.println("result = " + result);
+        Scanner scanner = new Scanner(System.in);
+        long a = scanner.nextLong();
+        long b = scanner.nextLong();
+        long c = scanner.nextLong();
+        System.out.println((a + b + c));
     }
 }
---
+++ src/b11382.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class b11382 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("a 입력");
+        int a = scanner.nextInt();
+        System.out.println("b 입력");
+        int b = scanner.nextInt();
+        System.out.println("c 입력");
+        int c = scanner.nextInt();
+        System.out.println("a+b+c = " + (a + b + c));
+    }
+}
