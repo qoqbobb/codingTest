ddbdbe5af6643cd325c31ede5e2dc363cff24fd9
qoqbobb <kail853@naver.com>
2025-12-29 09:17:12 +0900

    connect test

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        long a = scanner.nextLong();
-        long b = scanner.nextLong();
-        long c = scanner.nextLong();
-        System.out.println((a + b + c));
+        System.out.println("TEST");
     }
 }
